9d4236b861e2ed28590535bad6cd56a7cbeefb05
omardevelops <omarahmadp91@gmail.com>
2022-07-03 17:27:36 +0400

    added: ship factory + tests

--- src/__tests__/Ship.test.js
+++ src/__tests__/Ship.test.js
@@ -1,1 +1,50 @@
 import Ship from '../factories/Ship';
+
+test('Ship with length 2 is initialized', () => {
+  const ship = Ship(2);
+  expect(ship.length).toBe(2);
+  expect(ship.hitPos).toStrictEqual([0, 0]);
+});
+
+test('Ship with length 4 is initialized', () => {
+  const ship = Ship(4);
+  expect(ship.length).toBe(4);
+  expect(ship.hitPos).toStrictEqual([0, 0, 0, 0]);
+});
+
+// HitAt Position is mapped to the ship length
+test('hitAt position 2', () => {
+  const ship = Ship(3);
+  ship.hitAt(2);
+  expect(ship.hitPos).toStrictEqual([0, 0, 1]);
+});
+
+test('hitAt position 0 and position 2', () => {
+  const ship = Ship(3);
+  ship.hitAt(0);
+  ship.hitAt(2);
+  expect(ship.hitPos).toStrictEqual([1, 0, 1]);
+});
+
+test('hitAt out of bounds over length', () => {
+  const ship = Ship(2);
+  expect(() => ship.hitAt(3)).toThrow();
+});
+
+test('hitAt out of bounds less than 0', () => {
+  const ship = Ship(2);
+  expect(() => ship.hitAt(-3)).toThrow();
+});
+
+test('isSunk works correctly', () => {
+  const ship = Ship(2);
+  ship.hitAt(0);
+  ship.hitAt(1);
+  expect(ship.isSunk()).toBe(true);
+});
+
+test('isSunk for still alive ship', () => {
+  const ship = Ship(6);
+  ship.hitAt(3);
+  expect(ship.isSunk()).toBe(false);
+});
--- src/factories/Ship.js
+++ src/factories/Ship.js
@@ -1,11 +1,15 @@
 const Ship = (len) => {
   const length = len;
-  let hitPos = []; // 0 is unhit, and 1 is hit
+  let hitPos = Array(len).fill(0); // 0 is unhit, and 1 is hit
 
-  const hit = (pos) => -1;
-  const isSunk = () => -1;
+  const hitAt = (pos) => {
+    if (pos >= length || pos <= -1) throw new Error('invalid'); // check for out of bounds
+    hitPos[pos] = 1;
+  };
+  const isSunk = () =>
+    hitPos.filter((hit) => hit !== 1).length === 0 ? true : false; // only returns true when the array has no 0s left
 
-  return { length, hit, isSunk };
+  return { length, hitPos, hitAt, isSunk };
 };
 
 export default Ship;
