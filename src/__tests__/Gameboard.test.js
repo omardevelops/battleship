7fc419ca67627ade099f80847032f4925a79b985
omardevelops <omarahmadp91@gmail.com>
2022-07-06 12:17:42 +0400

    added: refactoring of placing ship + checks

--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -19,28 +19,116 @@ test('Creates and registers ships', () => {
   });
 });
 
-test('Places a ship on an empty grid horizontally (1)', () => {
+test('Places a ship on an empty grid x-axis (1)', () => {
   const board = Gameboard(5);
   const ship = Ship(4);
   board.registerShip(ship);
-  board.placeShipOnGrid(1, 0, 'x');
+  board.placeShipOnGrid(1, { x: 1, y: 1 }, 'x');
   expect(board.grid).toEqual([
     [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
   ]);
 });
 
-test.skip('Places a ship on an empty grid horizontally (2)', () => {
+test('Places a ship on an empty grid x-axis (2)', () => {
+  const board = Gameboard(5);
+  const ship = Ship(3);
+  board.registerShip(ship);
+  board.placeShipOnGrid(1, { x: 1, y: 2 }, 'x');
+  expect(board.grid).toEqual([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ]);
+});
+
+test('Places a ship on an empty grid x-axis (3)', () => {
   const board = Gameboard(5);
   const ship = Ship(4);
   board.registerShip(ship);
-  board.placeShipOnGrid(1, 0, 'x');
+  board.placeShipOnGrid(1, { x: 0, y: 4 }, 'x');
   expect(board.grid).toEqual([
-    1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0],
   ]);
+});
+
+test('Does not place a ship on a preexisting ship location', () => {
+  const board = Gameboard(5);
+  const ship1 = Ship(4);
+  const newShip = Ship(3);
+  board.registerShip(ship1);
+  board.registerShip(newShip);
+  board.placeShipOnGrid(1, { x: 0, y: 4 }, 'x');
+  board.placeShipOnGrid(2, { x: 2, y: 4 }, 'x');
+  expect(board.grid).toEqual([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0],
+  ]);
+});
+
+test('Does not place a ship near preexisting ships (x-axis)', () => {
+  const board = Gameboard(5);
+  const ship1 = Ship(4);
+  const newShip = Ship(3);
+  board.registerShip(ship1);
+  board.registerShip(newShip);
+  board.placeShipOnGrid(1, { x: 0, y: 4 }, 'x');
+  board.placeShipOnGrid(2, { x: 1, y: 3 }, 'x');
+  expect(board.grid).toEqual([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0],
+  ]);
+});
+
+test('Placing ship out of bounds throws (x)', () => {
+  const board = Gameboard(5); // 5x5 grid
+  const ship = Ship(4);
+  board.registerShip(ship);
+  expect(() => board.placeShipOnGrid(1, { x: 5, y: 0 }, 'x')).toThrow(
+    'input x coordinate out of bounds'
+  );
+});
+
+test('Placing ship out of bounds throws (x and y)', () => {
+  const board = Gameboard(5); // 5x5 grid
+  const ship = Ship(4);
+  board.registerShip(ship);
+  expect(() => board.placeShipOnGrid(1, { x: 5, y: 7 }, 'x')).toThrow(
+    'input y coordinate out of bounds'
+  );
+});
+
+test('Placing ship longer than allowed does nothing (x-axis)', () => {
+  const board = Gameboard(5); // 5x5 grid
+  const ship = Ship(3);
+  board.registerShip(ship);
+  expect(() => board.placeShipOnGrid(1, { x: 3, y: 1 }, 'x')).toThrow(
+    'Ship longer than allocated space (x-axis)'
+  );
+});
+
+test('Placing ship longer than allowed does nothing (y-axis)', () => {
+  const board = Gameboard(5); // 5x5 grid
+  const ship = Ship(3);
+  board.registerShip(ship);
+  expect(() => board.placeShipOnGrid(1, { x: 1, y: 3 }, 'y')).toThrow(
+    'Ship longer than allocated space (y-axis)'
+  );
 });
 
 test.skip('meow', () => {
--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -1,8 +1,35 @@
+import { getHostname } from 'webpack-dev-server';
+
 const GRID_SIZE = 10; // 10x10 grid size is standard
 const Gameboard = (gridSize) => {
   let shipRegistry = {}; // Keeps track of Ship objects by indexing them
   let latestShipIndex = 1;
   let grid = []; // multi dimensional array when initialized
+
+  const isPlacingShipAllowed = (shipLength, startPos, endPos, axis) => {
+    if (startPos.y >= grid.length)
+      throw new Error('input y coordinate out of bounds');
+    else if (startPos.x >= grid[startPos.y].length)
+      throw new Error('input x coordinate out of bounds');
+
+    if (axis === 'x') {
+      if (endPos.x >= grid[endPos.y].length)
+        throw new Error('Ship longer than allocated space (x-axis)');
+    } else if (axis === 'y') {
+      if (endPos.y >= grid.length)
+        throw new Error('Ship longer than allocated space (y-axis)');
+    }
+
+    // Checks the area in and around the ship. If not empty, returns false.
+    for (let y = startPos.y - 1; y <= startPos.y + 1; y++) {
+      if (grid[y] === undefined) continue; // skip loop for out of range coordinate (edge cases)
+      let row = grid[y];
+      for (let x = startPos.x - 1; x <= startPos.x + 1; x++) {
+        if (row[x] !== 0 && row[x] !== undefined) return false;
+      }
+    }
+    return true;
+  };
 
   const registerShip = (ship) => (shipRegistry[latestShipIndex++] = ship);
   const initializeGrid = () => {
@@ -15,12 +42,26 @@ const Gameboard = (gridSize) => {
   };
   const placeShipOnGrid = (shipIndex, startPos, axis) => {
     const ship = shipRegistry[shipIndex];
+    let endPos = {};
+
     if (axis === 'x') {
-      const endPos = startPos + ship.length - 1; // end position is inclusive
-      for (let i = startPos; i <= endPos; i++) {
-        // grid[i] = shipIndex;
-      }
+      endPos.y = startPos.y;
+      endPos.x = startPos.x + ship.length - 1; // end position is inclusive
     } else if (axis === 'y') {
+      endPos.x = startPos.x;
+      endPos.y = startPos.y + ship.length - 1; // end position is inclusive
+    }
+
+    if (isPlacingShipAllowed(ship, startPos, endPos, axis)) {
+      if (axis === 'x')
+        for (let i = startPos[axis]; i <= endPos[axis]; i++) {
+          grid[startPos.y][i] = shipIndex;
+        }
+      else if (axis === 'y')
+        for (let i = startPos[axis]; i <= endPos[axis]; i++) {
+          grid[i][startPos.x] = shipIndex;
+        }
+      console.log(grid);
     }
   };
 
