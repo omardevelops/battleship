7a655ecaa5adfdc561660c6fb16ae80d2cdff5b7
omardevelops <omarahmadp91@gmail.com>
2022-07-07 07:40:46 +0400

    refactored hitting ship

--- src/__tests__/Ship.test.js
+++ src/__tests__/Ship.test.js
@@ -7,48 +7,46 @@ test('Ship with negative length throws', () => {
 test('Ship with length 2 is initialized', () => {
   const ship = Ship(2);
   expect(ship.length).toBe(2);
-  expect(ship.hitPos).toStrictEqual([0, 0]);
 });
 
-test('Ship with length 4 is initialized', () => {
+test('Hit function works', () => {
   const ship = Ship(4);
-  expect(ship.length).toBe(4);
-  expect(ship.hitPos).toStrictEqual([0, 0, 0, 0]);
+  expect(ship.hit()).toBe(3);
 });
 
-// HitAt Position is mapped to the ship length
-test('hitAt position 2', () => {
-  const ship = Ship(3);
-  ship.hitAt(2);
-  expect(ship.hitPos).toStrictEqual([0, 0, 1]);
-});
+// // HitAt Position is mapped to the ship length
+// test('hitAt position 2', () => {
+//   const ship = Ship(3);
+//   ship.hitAt(2);
+//   expect(ship.hitPos).toStrictEqual([0, 0, 1]);
+// });
 
-test('hitAt position 0 and position 2', () => {
-  const ship = Ship(3);
-  ship.hitAt(0);
-  ship.hitAt(2);
-  expect(ship.hitPos).toStrictEqual([1, 0, 1]);
-});
+// test('hitAt position 0 and position 2', () => {
+//   const ship = Ship(3);
+//   ship.hitAt(0);
+//   ship.hitAt(2);
+//   expect(ship.hitPos).toStrictEqual([1, 0, 1]);
+// });
 
-test('hitAt out of bounds over length', () => {
-  const ship = Ship(2);
-  expect(() => ship.hitAt(3)).toThrow();
-});
+// test('hitAt out of bounds over length', () => {
+//   const ship = Ship(2);
+//   expect(() => ship.hitAt(3)).toThrow();
+// });
 
-test('hitAt out of bounds less than 0', () => {
-  const ship = Ship(2);
-  expect(() => ship.hitAt(-3)).toThrow();
-});
+// test('hitAt out of bounds less than 0', () => {
+//   const ship = Ship(2);
+//   expect(() => ship.hitAt(-3)).toThrow();
+// });
 
 test('isSunk works correctly', () => {
   const ship = Ship(2);
-  ship.hitAt(0);
-  ship.hitAt(1);
+  ship.hit();
+  ship.hit();
   expect(ship.isSunk()).toBe(true);
 });
 
 test('isSunk for still alive ship', () => {
   const ship = Ship(6);
-  ship.hitAt(3);
+  ship.hit();
   expect(ship.isSunk()).toBe(false);
 });
