bc13bcd44b6b68207123876cbf48f21c3ecb76a1
omardevelops <omarahmadp91@gmail.com>
2022-07-08 14:42:26 +0400

    add: Player computer auto attack

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -5,7 +5,14 @@ const Player = () => {
     return enemyBoard.receiveAttack({ x, y });
   };
 
-  return { isComputerPlayer, attack };
+  const computerAttack = (enemyBoard) => {
+    const gridSize = enemyBoard.grid.length;
+    const x = Math.floor(Math.random() * gridSize);
+    const y = Math.floor(Math.random() * gridSize);
+    attack({ x, y }, enemyBoard);
+  };
+
+  return { isComputerPlayer, attack, computerAttack };
 };
 
 export default Player;
