1e6f165bf9a0f287790b43faa945411e58955394
omardevelops <omarahmadp91@gmail.com>
2022-07-17 12:36:11 +0400

    added: grid UI html and styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './reset.css';
 import './style.css';
+
+const grid1 = document.querySelector('#player1-grid');
+const grid2 = document.querySelector('#player2-grid');
+for (let index = 0; index < 100; index++) {
+  const div = document.createElement('div');
+  grid1.appendChild(div);
+  const div2 = document.createElement('div');
+  grid2.appendChild(div2);
+}
